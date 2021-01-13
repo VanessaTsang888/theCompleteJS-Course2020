c91884d81bd8f02cf37fd841900954ef68a3c34a
Vanessa Tsang <vanessatsang@cic001069.home>
2021-01-13 16:01:14 +0000

    Now completed lecture 32 Activating Strict Mode

--- Version-02/02-Fundamentals-Part-2/starter/script.js
+++ Version-02/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,59 @@
-/*** Dynamic Typing in action ***/
+// This is to Activate strict mode that make it easier for me to write secure JavaScript code:
+'use strict';
+
+/****************************************************************************************************************************************************************** 
+
+31. SECTION INTRO:
+
+Activate strict mode: wrting secure code.
+a special mode we can activate in JavaScript makes it easy for us to write secure JavaScript code.
+Write this string at the begining of the script.js file:
+'use strict';
+Makes it easier for developers to avoid accidental errors / bugs into our code as:
+strict mode will forbids us to do certain things, and second it will actually create visible errors for us in certain situations in which without strct mode 
+JavaScript will fail silently without letting us know we made an mistake.
+
+******************************************************************************************************************************************************************/
+
+/*******************************************************************************************************************************************************************
+// Most imporant changes strict mode has: use const to declare variable unless I'm sure the value of the variable will change.
+
+let hasDriversLicense = false; // Once the person has passed their test, then this variable will be set to: true
+const passTest = true;
+
+// The variable passTest will be set to true or false:
+// // on purpose omit the 's' from the variable name, and without the 'use strict' activated, JavaScript will not know that I've created a bug.
+// 
+
+if (passTest) hasDriversLicense = true; 
+// This one has the correct spelling of the variable name, but since the variable name above is missing the letter 's' from the variable name, this will NOT logout
+// the below String. This is due to the mistake I created on purpose and the variable is still set to false.
+// Without the 'use strict' statement activated, I would not get the error message in the console as JavaScript wouldn't know about the bug I've created.
+if (hasDriversLicense) console.log('I can drive');
+
+
+// Another thing that strict mode do is to introduce a short list of variable names that are reserved for features that might be added to the language a bit later.
+// JavaScript has reserved the variable that it might implement in the future. This is why I get Syntax Error when I logout this line of code.
+
+// const interface = 'Audio'; // Uncaught SyntaxError: Unexpected strict mode reserved word
+// Another example:
+// Again this variable will logout an Error as strict mode reserves some name for the future.
+// const private = 534;
+
+// Same logic for this one.
+const if = 23; // Uncaught SyntaxError: Unexpected token 'if'
+
+// From now on in this course, I have to have strict mode activated.
+
+*******************************************************************************************************************************************************************/
+
+
+
+
+
+/*******************************************************************************************************************************************************************
+
+Dynamic Typing in action 
 
 // We can easily change the data type of a value that is held by a variable at anytime
 
@@ -32,3 +87,5 @@ console.log(typeof year); // will logout: number.
 // null is similar to Undefined as both the value and the type of the value are null.
 // The typeo null is an Object. Should really return null but there is a bug. Make sure you don't create any accidentle bugs due to this strange behaviour.
 console.log(typeof null);
+
+*******************************************************************************************************************************************************************/
