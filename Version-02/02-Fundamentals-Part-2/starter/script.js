e430e48df90784f8bdae4cff9d57f87c08d54ab4
Vanessa Tsang <vanessatsang@cic001069.home>
2021-01-13 22:10:47 +0000

    Completed lecture 33 Functions

--- Version-02/02-Fundamentals-Part-2/starter/script.js
+++ Version-02/02-Fundamentals-Part-2/starter/script.js
@@ -55,11 +55,13 @@ A function is a piece of code that we can reuse over and over again in our app. 
 A function hold one or more complete lines of code.
 When using functions, I am using the Principle 'Don't Repeat Yourself' or DRY. We say that we should keep our code DRY.
 
+A function not only can reuse a piect of code but can also receive data and return data back.
+
 Example, fruitProcessor function to produce apple fruit or apple and orange juice.
 
 ***************************************************************************************************************************************************************/
 
-
+// Logic of reusing code:
 // This function is created by using the 'function' keyword. This function is called: logger, which is a function that will log something to the console.
 // We need a set of empty parenthises and a pair of braces to create the function body which will get executed when I run this function later. 
 // Lets say I need to execute the log many times in the app. 
@@ -74,6 +76,54 @@ function logger() {
 logger(); // My name is Vanessa
 logger(); // My name is Vanessa
 logger(); // My name is Vanessa
+
+// lione 67 was executed three times.
+
+// When we write function we pass data into a function, and a function can also return data as well for me to use for something else in the app. so,
+// not only can a function reuse a piect of code but can also receive data and return data back. Functions are like machines.
+// Example, fruit processor: we put fruit into the processor do something to our fruit which is the function body and then the processor returns the processed
+// fruit like juice.
+// In this function I specify two parameters which are like variables that are specific to this function and they will get defined when we call them later.
+// The two parameters need to be separated by commas and they reprsent the input data of this function.
+function fruitProcessor(apples, oranges) {
+    // using the parameters just as they are normal variables
+    // console.log(apples, oranges); // 5 0
+    // do something with these parameters and produce some juice by creating a String. We build a string using the input data that we get in the function 
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    // we can use the return keyword and can return any value from the function. Lets return the juice that we just produced
+    return juice; // this juice will become the result of executing this function.
+
+}
+
+// Call the fruitProcessor function now. 5 apples and 0 oranges. 
+// Apples will become 5, and oranges which is the second parameter will become 0. These actual values of the parameters are called arguments.
+// coming from line 90
+// fruitProcessor(5, 0) // 5 0
+// Will logout: 5 0
+// As that is the numbers I specified when calling the fruitProcessor function.
+
+// The result of running this function is the juice that we just returned.
+// Calling the function code will be replaced by the juice string. So if we want to use the value that was returned we need to store it in a variable.
+
+// So we call the fruitProcessor function with two arguments, 5 and 0 which are specific so the actual values of the functions parameters which are apples
+// and oranges. So when the function is running now, apples (inside the console.log) will be 5, and oranges will be 0. Then we use these values to build the
+// juice string. Then we return that value from the function. The result of calling the function will be the juice value that was just returned. Then we need
+// to save that value somewhere, capture it by saving it into the appleJuice variable. 
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice); // Juice with 5 apples and 0 oranges.
+// Then log that value directly to the console. Now, its executed/logout same string twice.
+// console.log(fruitProcessor(5, 0));
+// console.log(appleJuice);
+
+// we can reuse the function with different input values and then get a different output.
+// Lets create apple and orange juice:
+// I am defining specific values which are the arguments which will get passed into the function as the parameters. Essentially, I created a generic funtion
+// that works for any number of apples and oranges, then when i call the function I'm filling in the blank spaces using my arguments. 
+const appleOrangeJuice = fruitProcessor(2, 4);
+// Now can log the result:
+console.log(appleOrangeJuice);
+
+// Not all functions need to return something or acept parameters but unusal.
 
 
 
