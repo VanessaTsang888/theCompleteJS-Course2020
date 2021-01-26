324ac487dd3c55adc115a51b2066af722f8995e5
Vanessa Tsang <vanessatsang@cic001069.home>
2021-01-26 18:10:27 +0000

    Made a start on 50. Coding Challenge 4

--- Version-02/02-Fundamentals-Part-2/starter/codingChallenges.js
+++ Version-02/02-Fundamentals-Part-2/starter/codingChallenges.js
@@ -218,6 +218,8 @@ Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 // 2. Create a 'calcBMI'method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from
 // the method.
 // The function don't need any parameters as its taking the values directly fromt he object.
+
+/*
 const mark = {
     fullName: 'Mark Miller',
     mass: 78,
@@ -257,6 +259,60 @@ if (mark.bmi > john.bmi) {
 } else if (john.bmi > mark.bmi) {
     console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
 }
+*/
+
+/******************************************************************************************************************************************* 
+
+Coding Challenge 4: loops:
+
+Let's improve Steven's tip calculator even more, this time using loops!
+
+
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the
+bills array. Use a for loop to perform the 10 calculations!
+
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays
+
+
+**********************************************************************************************************************************************/
+
+// 1. Create an array 'bills' containing all 10 test bill values:
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals'):
+const tips = [];
+const totals = [];
+
+// 3. 
+// Calculate both tips and the total values for every single bill value that's in the bills array in Q1.
+const calcTip = function (bills) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+// for loop to perform the 10 caculations:
+
+// Another example: an array of birthYears. Calculate the ages for each birthYear and store them in a new array:
+// Create empty array which will then hold the ages. Loop through the birthYears then fill-up this ages array.
+// Then calculate the age of the current year. The year we are currently in is 2037, then minus the birth year.
+// Then we want to push it to that empty array.
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(bills[i]);
+    totals.push(bills[i] + tips[i]);
+}
+
+console.log(tips, totals); // 
 
 
 
