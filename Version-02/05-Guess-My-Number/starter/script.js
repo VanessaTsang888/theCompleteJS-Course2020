cf506501c899878fe36f34cec143e494f9eae3c8
Vanessa Tsang <vanessatsang@cic001069.home>
2021-01-27 16:16:57 +0000

    Started on Section 7. JavaScript in the Browser: DOM and Events Fundamentals

---
+++ Version-02/05-Guess-My-Number/starter/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**************************************************************************************************************************************** 
+
+SECTION 7. JavaScript in the Browser: DOM and Events Fundamentals.
+
+70. Project 1: Gues My Number!
+
+
+
+************************************************************************************************************************************** */
+
+// In JavaScript we can select elements based on it's class (see index.html file).
+// Select an element in JavaScript - a class
+console.log(); document.querySelector('.message');
+
+// Open the live server in Terminal.
+// Open the Terminal tab her, use this command: live-server
+// Then hit Enter key.
+
+
