c48a9f80b7ee7364e74642593a66461f32edba45
Vanessa Tsang <vanessatsang@cic001069.home>
2021-01-27 17:08:28 +0000

    Completed lecture 70

--- Version-02/05-Guess-My-Number/starter/script.js
+++ Version-02/05-Guess-My-Number/starter/script.js
@@ -4,18 +4,21 @@
 
 SECTION 7. JavaScript in the Browser: DOM and Events Fundamentals.
 
-70. Project 1: Gues My Number!
+70. Project 1: Guess My Number!
 
+***************************************************************************************************************************************/
 
-
-************************************************************************************************************************************** */
-
+// QUICK INTRO TO THE DOM AND HOW TO SELECT AN ELEMENT:
 // In JavaScript we can select elements based on it's class (see index.html file).
 // Select an element in JavaScript - a class
-console.log(); document.querySelector('.message');
+// executed from left to right. The .textContent is text: Start guessing...
+console.log(document.querySelector('.message').textContent);
 
+// First DOM manipulation:
 // Open the live server in Terminal.
 // Open the Terminal tab her, use this command: live-server
 // Then hit Enter key.
+
+/
 
 
