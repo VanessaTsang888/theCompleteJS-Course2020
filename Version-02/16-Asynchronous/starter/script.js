9b1bc63611b2c7fc9c33220bfae7d9c4241129bd
Vanessa Tsang <vantaidesign8@gmail.com>
2021-02-23 15:08:22 +0000

    Completed lecture 241

---
+++ Version-02/16-Asynchronous/starter/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+/*************************************************************************************************************************************************** 
+
+241. Section Intro
+
+Important JS features:
+
+Asynchronous JS:
+The goals of this is to deal with long running tasks that run in the background.
+The most common use case for Asynchronous JS is to fetch from remote servers, in AJAX calls. 
+
+Will learn about Promises, the fetch function, async await, error handling. 
+
+*****************************************************************************************************************************************************/
