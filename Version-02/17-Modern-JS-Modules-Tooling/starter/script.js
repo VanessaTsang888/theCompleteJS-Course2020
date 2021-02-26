9d725716d67fd69766271f67150db266bde3a1f4
Vanessa Tsang <vantaidesign8@gmail.com>
2021-02-26 16:05:16 +0000

    Completed lecture 264

---
+++ Version-02/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,12 @@
+// This not including in start file but I've included for Secure Coding purposes and hope it will not cause me any issues?
+'use strict';
+
+/*********************************************************************************************************************************************************** 
+
+264. Section Intro:
+
+Building JS apps in the real world. Not about the language itself, but more about the development process, the modern build tools, the whole modern ecosystem
+that has been created around JS 
+
+***********************************************************************************************************************************************************/
+
